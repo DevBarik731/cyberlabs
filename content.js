03672c433181874d72e4f80895d8f9e9ca82472f
DevBarik731 <barikbiswajit731@gmail.com>
2025-12-27 18:42:43 +0530

    Highlight Area UI Improvement

--- content.js
+++ content.js
@@ -1,8 +1,8 @@
 let loading=true
 let BmArray=[]
-// const GROQ_API_KEY=""
-// const GEMINI_API_KEY=""
-// const DEEPSEAK_API_KEY=""
+***REMOVED***
+const GEMINI_API_KEY="gemini api key"
+const DEEPSEAK_API_KEY="deepSeak api key"
 
 const BookMarks = document.createElement("div")
 BookMarks.id="BookMarks"
